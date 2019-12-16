9d54ecad7fe2dfa02f378248b8acd587ebc71e48
amit rai <bipulamit@gmail.com>
2019-12-16 19:52:30 +0530

    initial commit

---
+++ src/screens/home/index.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ToastAndroid} from 'react-native';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {loadHome} from './HomeActions';
+import AppHeader from '../../header/AppHeader';
+
+export class home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View>
+        <AppHeader title={'Home'} />
+        <Text>hello from home</Text>
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const {isFetching, error, data, success, failure} = state.HomeReducer;
+  return {
+    isFetching,
+    error,
+    data,
+    success,
+    failure,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    loadHome: bindActionCreators(loadHome, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(home);
+const styles = StyleSheet.create({
+  container: {},
+});
